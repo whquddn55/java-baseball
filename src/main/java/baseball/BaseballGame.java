e16ff201d08e6970c2ecd1d0f3d0de10a8ee2d6c
조병우 <whquddn55@gmail.com>
2022-11-02 22:02:20 +0900

    feat(validation): implement validateInputLength method
    Implement validateInputLength method in  BaseballGame class. This method validate size of integer list is INPUT_SIZE that is now 3.
    
    BREAKING CHANGE:
    Added following 1 method.
    
    void validateInputLength(List<Integer>);

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BaseballGame {
+    private static final int INPUT_SIZE = 3;
 
     public void run() throws IllegalArgumentException {
 
@@ -20,6 +21,12 @@ public class BaseballGame {
     private void validateIntegerRange(List<Integer> input) {
         if (input.stream().anyMatch(number -> number < 0 || number > 9)) {
             throw new IllegalArgumentException("입력이 [0, 9]사이의 숫자 이외의 문자를 포함합니다.");
+        }
+    }
+
+    private void validateInputLength(List<Integer> input) throws IllegalArgumentException {
+        if (input.size() != INPUT_SIZE) {
+            throw new IllegalArgumentException(String.format("입력한 숫자의 길이가 %d이 아닙니다.", INPUT_SIZE));
         }
     }
 }
